d95868eee732946c0fb01df2fed9e82cef1782d8
Brian Arroyo <brianarroyo@college.harvard.edu>
2015-07-27 23:30:36 -0400

    Added marking functionality to allow players to guess at treasure locations. added bounds checking. added second form for second player. some css changes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,25 @@
 "use strict";
 
-	var bw = 400;
-	var bh = 400;
-	var p = 10;
+var bw = 500;
+var bh = 500;
+var p = 0;
+var minBury = 1;
+var treasures = [];
+var buried = 0;
 
 
 function init() {
-
-
- // 	var graphics = new createjs.Graphics().beginFill("DeepSkyBlue").drawRect(0, 0, 100, 100);
-	// var rect = new createjs.Shape(graphics);
-
- //    var stage = new createjs.Stage("canvas");
- //    stage.addChild(rect);
- //    stage.update();
-
     var canvas = $("#canvas")[0];
 	var context = canvas.getContext("2d");
-	for (var x = 0; x <= bw; x += 40) {
-	    context.moveTo(0.5 + x + p, p);
-	    context.lineTo(0.5 + x + p, bh + p);
+	for (var x = 0; x <= bw; x += 50) {
+	    context.moveTo(0.5 + x, p);
+	    context.lineTo(0.5 + x, bh + p);
 	}
 
 
-	for (var x = 0; x <= bh; x += 40) {
-	    context.moveTo(p, 0.5 + x + p);
-	    context.lineTo(bw + p, 0.5 + x + p);
+	for (var x = 0; x <= bh; x += 50) {
+	    context.moveTo(p, 0.5 + x);
+	    context.lineTo(bw, 0.5 + x);
 	}
 
 	context.strokeStyle = "black";
@@ -34,13 +28,32 @@ function init() {
 
 function bury() {
 	var values = {};
+	// extract values from the form
 	$.each($('#loc').serializeArray(), function(i, field) {
     	values[field.name] = field.value;
 	});
+
 	var x = values.xcor;
 	var y = values.ycor;
 
-	// var stage = new createjs.Stage("canvas");
+	if (x > 9 || y > 9 || x < 0 || y < 0) {
+		alert("Sorry, but those coordinates are out of range!");
+		return;
+	}
+
+	// logicing the coordinates, adding to dictionary
+	if (treasures[x,y]){
+		alert("You have already buried a treasure here! Try again!");
+		return;
+	}
+
+	treasures[[x,y]] = true;
+	buried++;
+	var x = x * 50 + 25;
+	var y = y * 50 + 25;
+
+	// get the canvas and draw the circle. have yet to determine the logic for
+	// placing the circle
 	var canvas = $("#canvas")[0].getContext("2d");
 	canvas.beginPath();
 	canvas.arc(x, y, 20, 0, 2 * Math.PI, false);
@@ -49,14 +62,33 @@ function bury() {
 	canvas.lineWidth = 2;
 	canvas.strokeStyle = '#003300';
 	canvas.stroke();
-	// console.log(typeof(canvas));
-	// console.log(typeof(stage));
+}
 
-	// var circle = new createjs.Shape();
-    // circle.graphics.beginFill("red").drawCircle(x, y, 20);
-    //Set position of Shape instance.
-    // stage.addChild(circle);
-    // stage.addChild(canvas);
-    //Update stage will render next frame
-    // stage.update();
+function gameBegin() {
+	if (buried < minBury) {
+		alert("You have yet to bury some treasures! You have " + (minBury - treasures.length) + " left to bury.");
+		return;
+	}
+
+	$('#loc')[0].style.display="none";
+	$('#hunt')[0].style.display="block";
+
+	for (var trunk in treasures) {
+		trunk = trunk.split(",");
+		eraseCircle(trunk);
+	}
+
+}
+
+// used to uncover treasure
+function eraseCircle(xy){
+	var canvas = $("#canvas")[0].getContext("2d");
+	canvas.beginPath();
+	canvas.arc(xy[0] * 50 + 25, xy[1] * 50 + 25, 10, 0, 2 * Math.PI, false);
+	canvas.fillStyle = 'red';
+	canvas.fill();
+	canvas.lineWidth = 2;
+	canvas.strokeStyle = '#003300';
+	canvas.stroke();
+
 }
